24c18e9227d053db0ed65b348edac237705c959d
dabideee13 <d.e.magno13@gmail.com>
2021-07-13 14:50:13 +0800

    Added problem statement

--- mod2_act1.py
+++ mod2_act1.py
@@ -1,5 +1,25 @@
 # mod2_act1.py
 
+"""
+farenheit = 120.25
+celsius = 37.38
+
+celsius_to_fahrenheit = ###
+fahrenheit_to_celsius = ###
+
+print(celsius, "celsius is", round(celsius_to_fahrenheit, 2), "fahrenheit")
+print(fahrenheit, "fahrenheit is", round(fahrenheit_to_celsius, 2), "celsius")
+
+Tasks
+1. Modify the given code and assign the correct expressions to convert
+    fahrenheit to celsius and vice versa.
+
+Formula:
+    (__degree celsius * 9/5) + 32 = __degree fahrenheit
+    (__degree fahrenheit * 5/9) = __degree celsius
+
+"""
+
 
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * (9 / 5)) + 32
@@ -15,5 +35,5 @@ def fahrenheit_to_celsius(fahrenheit):
 fahrenheit = 120.25
 celsius = 37.38
 
-print(celsius, "celsius is", round(celsius_to_fahrenheit(celsius), 2))
-print(fahrenheit, "fahrenheit is", round(fahrenheit_to_celsius(fahrenheit), 2))
+print(celsius, "celsius is", round(celsius_to_fahrenheit(celsius), 2), "fahrenheit")
+print(fahrenheit, "fahrenheit is", round(fahrenheit_to_celsius(fahrenheit), 2), "celsius")
--- mod2_act2.py
+++ mod2_act2.py
@@ -1,5 +1,14 @@
 # mod2_act2.py
 
+"""
+Tasks
+1. Write a program code that allows the user to input values for a
+    salesperson's base salary, total sales, and commission rate. The
+    program calculates and displays the salesperson's pay by adding
+    the base salary to the product of the total sales and
+    commission rate.
+"""
+
 base_salary = float(input("Base salary: "))
 total_sales = float(input("Total sales: "))
 commission_rate = float(input("Commission rate: "))
