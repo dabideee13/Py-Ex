fe12024b11226ab58a892cf6aec210c14cc14828
dabideee13 <d.e.magno13@gmail.com>
2021-07-13 14:36:32 +0800

    Updated mod3_act2.py

--- mod3_act2.py
+++ mod3_act2.py
@@ -5,7 +5,7 @@
 2. The program will ask to enter first and second number.
 3. The program will display "The sum of n1 and n2 is nTotal"
 4. The program will ask if the user wants to try again. The user will
-input Y/y if Yes and N/n if No.
+    input Y/y if Yes and N/n if No.
 5. If Yes, refer to step 2.
 6. If No, the program will display "Thank you!"
 """
