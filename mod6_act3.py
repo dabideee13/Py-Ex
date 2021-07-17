3f0a85c86e305d6cea3768f286f6c32180933d9a
dabideee13 <d.e.magno13@gmail.com>
2021-07-17 19:03:56 +0800

    Added mod6_act3.py

--- mod6_act3.py
+++ mod6_act3.py
@@ -16,3 +16,41 @@ Tasks:
     6. Calling the switchOn() will automatically execute lightOpen()
         and ovenOpen()
 """
+
+
+class House:
+
+    def __init__(self, floorSize, noOfFloors, noOfDoors):
+        self.floorSize = floorSize
+        self.noOfFloors = noOfFloors
+        self.noOfDoors = noOfDoors
+
+        print(f"No of floors: {self.noOfFloors}")
+        print(f"No of doors: {self.noOfDoors}\n")
+
+    def switchOn(self):
+        print("switch on")
+        self.lightOpen()
+        self.ovenOpen()
+
+    def lightOpen(self):
+        print("light open")
+
+    def ovenOpen(self):
+        print("oven open")
+
+
+class TownHouse(House):
+
+    def __init__(self, noOfFloors, noOfDoors):
+        super().__init__(self, noOfFloors, noOfDoors)
+
+
+def main():
+
+    town_house = TownHouse(3, 120)
+    town_house.switchOn()
+
+
+if __name__ == '__main__':
+    main()
