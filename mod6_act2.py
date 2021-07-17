9396f6e3703d8f3bfb3f030221462c90e6d7fe5a
dabideee13 <d.e.magno13@gmail.com>
2021-07-17 18:34:49 +0800

    Added mod6_act2.py

--- mod6_act2.py
+++ mod6_act2.py
@@ -9,3 +9,33 @@ Tasks:
         information
     4. Display all the properties of the object
 """
+
+
+class Employee:
+
+    def __init__(self, name, email, mobile_number):
+        self.name = name
+        self.email = email
+        self.mobile_number = mobile_number
+
+        print(f"Name: {self.name}")
+        print(f"Email: {self.email}")
+        print(f"Mobile Number: {self.mobile_number}\n")
+
+
+def main():
+
+    emp1 = Employee(
+        'Azman Nads',
+        'azman.nads@g.msuiit.edu.ph',
+        '09061234567'
+    )
+    emp2 = Employee(
+        'Joseph Abordo',
+        'joseph.abordo@g.msuiit.edu.ph',
+        '09171234567'
+    )
+
+
+if __name__ == '__main__':
+    main()
