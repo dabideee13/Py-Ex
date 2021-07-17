4c1e9fd452f90b83e01f0be5d7ad12139a27a872
dabideee13 <d.e.magno13@gmail.com>
2021-07-17 18:13:03 +0800

    Added mod6_act1.py

--- mod6_act1.py
+++ mod6_act1.py
@@ -10,3 +10,29 @@ Tasks:
     2. Instansiate the Car class 3 times and display all properties
     3. Modify the properties
 """
+
+
+class Car:
+
+    def __init__(self, color, model, manufacturer):
+        self.color = color
+        self.model = model
+        self.manufacturer = manufacturer
+
+        print(f"Color: {self.color}")
+        print(f"Model: {self.model}")
+        print(f"Manufacturer: {self.manufacturer}\n")
+
+
+def main():
+
+    # Instantiate the Car class 3 times
+    Car1 = Car('white', '2020 Kia Telluride', 'Kia')
+    Car2 = Car('red', '2020 Chevrolet Corvette', 'Chevrolet')
+    Car3 = Car('gray', '2020 Jeep Gladiator', 'Jeep')
+
+
+if __name__ == '__main__':
+    main()
+
+
