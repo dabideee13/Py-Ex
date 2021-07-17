0111d054f906cc775c43e19b54854e149556ee59
dabideee13 <d.e.magno13@gmail.com>
2021-07-17 17:56:14 +0800

    Added mod5_act2.py

--- mod5_act2.py
+++ mod5_act2.py
@@ -11,3 +11,55 @@ Tasks:
     6. Use the appropriate Exception (ex: Invalid input such as text
         zero division)
 """
+
+
+def formatter(x):
+    print(f"Answer: {x}\n")
+
+
+def perform_operation(operation, first, second):
+
+    operation = operation.strip().lower()
+    operation_list = ['add', 'subtract', 'multiply', 'divide']
+
+    if operation not in operation_list:
+        print("Invalid input: operation not allowed\n")
+
+    if operation == 'add':
+        result = first + second
+        formatter(result)
+
+    elif operation == 'subtract':
+        result = first - second
+        formatter(result)
+
+    elif operation == 'multiply':
+        result = first * second
+        formatter(result)
+
+    elif operation == 'divide':
+        try:
+            result = first / second
+            formatter(result)
+        except ZeroDivisionError:
+            print("Invalid input: zero division error\n")
+
+
+yes_list = ['yes', 'y']
+no_list = ['no', 'n']
+
+while True:
+
+    operation = input("Add, Subtract, Multiply, or Divide: ")
+    first = float(input("Enter first number: "))
+    second = float(input("Enter second number: "))
+    perform_operation(operation, first, second)
+
+    trial = input("\nTry again? (Yes/No): ")
+
+    if trial.strip().lower() in yes_list:
+        continue
+
+    elif trial.strip().lower() in no_list:
+        break
+
